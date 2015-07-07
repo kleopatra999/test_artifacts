bca8e6d33c577720edf64a2bd74da26b7efe7381
Patrick Maupin <pmaupin@gmail.com>
2015-07-07 00:23:06 -0500

    Update MD5 checksums

--- cleanup.py
+++ cleanup.py
@@ -4,15 +4,19 @@ import sys
 import os
 import glob
 from pdfrw import PdfReader, PdfWriter
+sys.path.insert(0, '..')
+from autotest import checkmd5
 
-dirnames = sys.argv[1:] or sorted(os.listdir(build))
+
+dirnames = sys.argv[1:] or sorted(os.listdir('builds'))
 
 filelist = []
+extralist = []
 for name in dirnames:
     dirname = 'builds/%s' % name
     assert os.path.exists(dirname)
-    filelist += glob.glob('%s/*.pdf' % dirname)
     filelist += glob.glob('%s/*/*.pdf' % dirname)
+    extralist += glob.glob('%s/*/*/*.pdf' % dirname)
 
 def fixone(srcf, dstf):
     print(srcf)
@@ -35,17 +39,27 @@ def fixone(srcf, dstf):
             if not uri.startswith('file:///'):
                 continue
             a.URI = prefix + uri.split('rst2pdf/tests', 1)[-1]
-
     PdfWriter().write(dstf, trailer)
 
-for name in filelist:
-    if os.path.isdir(name):
-        continue
-    outname = name.replace('builds', 'fixed', 1)
-    outdir = os.path.dirname(outname)
-    if not os.path.exists(outdir):
-        os.makedirs(outdir)
-    try:
-        fixone(name, outname)
-    except Exception, s:
-        print s
+def fixstuff(mylist, update_md5):
+    for name in mylist:
+        outname = name.replace('builds', 'fixed', 1)
+        outdir = os.path.dirname(outname)
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
+        if not os.path.isdir(name):
+            try:
+                fixone(name, outname)
+            except Exception, s:
+                print s
+                continue
+        if update_md5:
+            md5name = os.path.basename(outname).replace('.pdf', '.json')
+            md5name = '../md5/%s' % md5name
+
+            result = checkmd5(name, md5name, [], False)
+            if result in ('good', 'bad'):
+                assert result == checkmd5(outname, md5name, [], result)
+
+fixstuff(extralist, False)
+fixstuff(filelist, True)
